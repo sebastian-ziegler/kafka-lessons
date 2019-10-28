82e005ff660a9e701c8dbeee4436162209d06154
sebastian-ziegler <sebastian.ziegler.m@gmail.com>
2019-10-28 23:40:08 +0100

    Kafka streams start

---
+++ kafka-streams-filter-tweets/src/main/scala/com/github/sebastianziegler/kafkalessons/kafkastreams/StreamsFilterTweets.scala
@@ -0,0 +1,37 @@
+package com.github.sebastianziegler.kafkalessons.kafkastreams
+
+import java.util.Properties
+
+import com.google.gson.JsonParser
+import org.apache.kafka.common.serialization.Serdes
+import org.apache.kafka.streams.kstream.KStream
+import org.apache.kafka.streams.{KafkaStreams, StreamsBuilder, StreamsConfig}
+import org.slf4j.LoggerFactory
+
+class StreamsFilterTweets {
+  val logger = LoggerFactory.getLogger(classOf[StreamsFilterTweets])
+  private def extractFollowersFromTweet(str: String): Int = {
+    Option(JsonParser.parseString(str).getAsJsonObject.get("user").getAsJsonObject.get("followers_count")).map(_.getAsInt).getOrElse(0)
+  }
+
+  def pullTweets(): Unit = {
+    val properties = new Properties()
+    properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092")
+    properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "demo-kafka-streams")
+    properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, classOf[Serdes.StringSerde].getName)
+    properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, classOf[Serdes.StringSerde].getName)
+
+    val streamsBuilder = new StreamsBuilder()
+
+    val inputTopic: KStream[String, String] = streamsBuilder.stream("twitter-topic")
+    val filteredStreams: KStream[String, String] = inputTopic.filter((k, jsonTweet) => {
+      extractFollowersFromTweet(jsonTweet) > 10000
+    })
+
+    filteredStreams.to("important_tweets")
+
+    val kafkaStreams = new KafkaStreams(streamsBuilder.build(), properties)
+
+    kafkaStreams.start()
+  }
+}
